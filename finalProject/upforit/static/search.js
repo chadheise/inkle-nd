4d0cd5bee57a410de955c10649b4d301c3f0c6d0
jwenger <jwenger@localhost>
2011-12-01 13:18:15 -0500

    Implemented search view.

--- finalProject/upforit/static/search.js
+++ finalProject/upforit/static/search.js
@@ -52,18 +52,56 @@ $(document).ready(function() {
     });
     
     $(".filterButton").click(function() {
-        var thisElement = $(this);
         
-        if (thisElement.val() == "All")
+        if ($(this).val() == "All")
         {
-             $(".filterButton").addClass("selected");          
+            $(".filterButton").addClass("selected");
+            
+            $("#searchContent").fadeOut(function() {
+                $("#people").show();
+                $("#locations").show();
+                $("#spheres").show();
+
+                $("#searchContent").fadeIn();
+            });
         }
         else
         {
-             $(".filterButton").removeClass("selected"); 
-             thisElement.addClass("selected");         
+            $(".filterButton").removeClass("selected");
+            $(this).addClass("selected");
+
+            if ($(this).val() == "People")
+            {
+                $("#searchContent").fadeOut(function() {
+                    $("#people").show();
+                    $("#locations").hide();
+                    $("#spheres").hide();
+
+                    $("#searchContent").fadeIn();
+                });
+            }
+            else if ($(this).val() == "Locations")
+            {
+                $("#searchContent").fadeOut(function() {
+                    $("#people").hide();
+                    $("#locations").show();
+                    $("#spheres").hide();
+
+                    $("#searchContent").fadeIn();
+                });
+            }
+            else if ($(this).val() == "Spheres")
+            {
+                $("#searchContent").fadeOut(function() {
+                    $("#people").hide();
+                    $("#locations").hide();
+                    $("#spheres").show();
+
+                    $("#searchContent").fadeIn();
+                });
+            }
         }
         
     });
-    
+   
 });
