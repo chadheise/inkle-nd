e47caf2439504c8920183777c6249e6ebbc9ca88
jwenger <jwenger@localhost>
2011-12-11 15:27:13 -0500

    cleaned up management screen, added fade effects when switching content, began implementation of spheres tab

--- finalProject/inkle/static/js/manage.js
+++ finalProject/inkle/static/js/manage.js
@@ -12,52 +12,36 @@ $(document).ready(function() {
         error: function(a, b, error) { alert(error); }
     });
     
+    function loadContent(URL) {
+        // Get Requests HTML
+        $.ajax({
+            type: "POST",
+            url: URL,
+            data: {},
+            success: function(html) {
+                $("#primaryContent").fadeOut("medium", function () {
+                    $("#primaryContent").html(html);
+                    $("#primaryContent").fadeIn("medium");
+                });
+            },
+            error: function(a, b, error) { alert(error); }
+        });
+        
+    }
+    
     $(".navButton").click(function() {
         var thisElement = $(this);
         $(".navButton").removeClass("selected")
         thisElement.addClass("selected")
 
         if ($(this).val() == "Requests")
-        {
-            // Get Requests HTML
-            $.ajax({
-                type: "POST",
-                url: "/inkle/requested/",
-                data: {},
-                success: function(html) {
-                    $("#primaryContent").html(html);
-                },
-                error: function(a, b, error) { alert(error); }
-            });
-        }
-        
+            { loadContent("/inkle/requested/") }
         else if ($(this).val() == "Circles")
-        {
-            // Get Requests HTML
-            $.ajax({
-                type: "POST",
-                url: "/inkle/circles/",
-                data: {},
-                success: function(html) {
-                    $("#primaryContent").html(html);
-                },
-                error: function(a, b, error) { alert(error); }
-            });
-        }
+            { loadContent("/inkle/circles/") }
+        else if ($(this).val() == "Spheres")
+            { loadContent("/inkle/spheres/") }
         else if ($(this).val() == "Followers")
-        {
-            // Get Requests HTML
-            $.ajax({
-                type: "POST",
-                url: "/inkle/followers/",
-                data: {},
-                success: function(html) {
-                    $("#primaryContent").html(html);
-                },
-                error: function(a, b, error) { alert(error); }
-            });
-        }
-            
+            { loadContent("/inkle/followers/") }
     });
 
     
