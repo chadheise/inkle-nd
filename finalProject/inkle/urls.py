f4b975e782474dc9f670439b952ddbf0c9674a2c
Jacob Wenger <wenger.jacob@gmail.com>
2011-12-12 01:01:05 -0500

    LOCATION BOARDS.'

--- finalProject/inkle/urls.py
+++ finalProject/inkle/urls.py
@@ -39,4 +39,5 @@ urlpatterns = patterns(
     (r"^getInklings/$", "get_inklings_view"),
     (r"^addSphere/$", "add_sphere_view"),
     (r"^addLocation/$", "add_location_view"),
+    (r"^populateLocationBoard/$", "populate_location_board_view"),
 )
