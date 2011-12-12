9de0fff6844d95cb3ffd67322d0a5e8cddebc33a
Chad Heise <7191739+chadheise@users.noreply.github.com>
2011-12-12 04:34:48 -0500

    pre-database populate

--- finalProject/inkle/forms.py
+++ finalProject/inkle/forms.py
@@ -14,10 +14,3 @@ class registration_form(forms.Form):
     password = forms.CharField(max_length = 20, label = "Password", widget = forms.PasswordInput)
     gender = forms.ChoiceField(choices = GENDERS, label = "Gender", widget = forms.Select(attrs = {}))
 
-#class recipe_form(forms.Form):
-#    category = forms.ChoiceField(choices=categories, label="Category", widget=forms.Select(attrs={}))
-#    title = forms.CharField(max_length=50)
-#    preparation = forms.CharField(max_length=5000, widget=forms.widgets.Textarea(), required=False)
-#    serving = forms.CharField(max_length=5000, widget=forms.widgets.Textarea(), required=False)
-#    notes = forms.CharField(max_length=5000, widget=forms.widgets.Textarea(), required=False)
-#    #card = forms.ImageField(required=False)
