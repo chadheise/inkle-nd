c92c3de0ea3a4fde50184b5a64e99db8c849747f
Jacob Wenger <wenger.jacob@gmail.com>
2011-12-19 16:10:08 -0500

    Added pending to requests page. Cleaned up requests page. Notification updates when requests change.

--- finalProject/inkle/static/js/memberCard.js
+++ finalProject/inkle/static/js/memberCard.js
@@ -58,44 +58,6 @@ $(document).ready(function() {
         }
     });
     
-    /*----------------------Accept Request Button --------------------------*/
-    $(".acceptRequest").live("click", function() {
-        var fromMemberID = parseInt($(this).attr("memberID"));
-
-        // Send friend request to database
-        $.ajax({
-            type: "POST",
-            url: "/inkle/acceptRequest/",
-            data: { "fromMemberID" : fromMemberID },
-            error: function (a, b, c) { alert(c); }
-        });
-        
-        var memberContainer = $(this).parent().parent();
-        memberContainer.fadeOut(function(){
-            memberContainer.html("Follow request accepted");
-            memberContainer.fadeIn();
-        });
-    });
-    
-    /*----------------------Reject Request Button --------------------------*/
-    $(".rejectRequest").live("click", function() {
-        var fromMemberID = parseInt($(this).attr("memberID"));
-
-        // Send friend request to database
-        $.ajax({
-            type: "POST",
-            url: "/inkle/rejectRequest/",
-            data: { "fromMemberID" : fromMemberID },
-            error: function (a, b, c) { alert(c); }
-        });
-        
-        var memberContainer = $(this).parent().parent();
-        memberContainer.fadeOut(function(){
-            memberContainer.html("Follow request rejected");
-            memberContainer.fadeIn();
-        });
-    });
-    
     /*----------------------Prevent Following Button --------------------------*/
     $(".preventFollowing").live("click", function() {
         var thisElement = $(this);
