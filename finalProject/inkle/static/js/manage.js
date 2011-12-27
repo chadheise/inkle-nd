37dc5f4d33e1f85351e5d4516c7ec2a7cf4d4b92
Jacob Wenger <wenger.jacob@gmail.com>
2011-12-27 15:33:48 -0500

    Initial load of manage content does not fade.

--- finalProject/inkle/static/js/manage.js
+++ finalProject/inkle/static/js/manage.js
@@ -1,46 +1,66 @@
 $(document).ready(function() {
     // Populate the main content with the initially selected main content link
     var contentType = $("#manageContentLinks .selectedContentLink").attr("contentType");
-    loadContent(contentType);
+    loadContent(contentType, true);
 
     /* Loads the content for the inputted content type and populates the main content with it */
-    function loadContent(contentType)
+    function loadContent(contentType, firstLoad)
     {
         $.ajax({
             type: "POST",
             url: "/inkle/" + contentType + "/",
             data: {},
             success: function(html) {
-                // Fade out the manage content, replace its HTML, and fade it back in
+                // If this is the first load, simply load the manage content
+                if (firstLoad)
+                {
+                    loadContentHelper(html);
+                }
+
+                // Otherwise, fade out the current manage content and fade the new manage content back in
+                else
+                {
                 $("#manageContent").fadeOut("medium", function () {
-                    // Show the requests content links if the content type is "requests"
-                    if (contentType == "requests")
-                    {
-                        // Show the requests content links
-                        $("#requestsContentLinks").show();
-
-                        // Set the "All" content link as the selected requests content link
-                        $("#requestsContentLink .selectedSubsectionContentLink").removeClass("selectedSubsectionContentLink");
-                        $("#requestsContentLinks p[contentType='all']").attr("id", "selectedSubsectionContentLink");
-                    }
-
-                    // Otherwise, hide the requests content links
-                    else
-                    {
-                        $("#requestsContentLinks").hide();
-                    }
-
-                    // Update the main content with the HTML returned from the AJAX call
-                    $("#mainManageContent").html(html);
-                    
-                    // Fade the manage content back in
-                    $("#manageContent").fadeIn("medium");
+                    loadContentHelper(html, function() {                    
+                        $("#manageContent").fadeIn("medium");
+                    });
                 });
+                }
             },
             error: function(a, b, error) { alert("manage.js (1): " + error); }
         });
     }
-   
+ 
+    /* Helper function for loadContent() which replaces the manage content HTML*/
+    function loadContentHelper(html, callback)
+    {
+        // Show the requests content links if the content type is "requests"
+        if (contentType == "requests")
+        {
+            // Show the requests content links
+            $("#requestsContentLinks").show();
+
+            // Set the "All" content link as the selected requests content link
+            $("#requestsContentLink .selectedSubsectionContentLink").removeClass("selectedSubsectionContentLink");
+            $("#requestsContentLinks p[contentType='all']").attr("id", "selectedSubsectionContentLink");
+        }
+
+        // Otherwise, hide the requests content links
+        else
+        {
+            $("#requestsContentLinks").hide();
+        }
+
+        // Update the main content with the HTML returned from the AJAX call
+        $("#mainManageContent").html(html);
+
+        // Execute the callback function if there is one
+        if (callback)
+        {
+            callback();
+        }
+    }
+
     /* Updates the main content when one of the main content links is clicked */
     $("#manageContentLinks p").click(function() {
         // Only update the content if the main content link which is clicked is not the currently selected one
@@ -52,7 +72,7 @@ $(document).ready(function() {
 
             // Load the content for the clicked main content link
             var contentType = $(this).attr("contentType");
-            loadContent(contentType);
+            loadContent(contentType, false);
         }
     });
     
