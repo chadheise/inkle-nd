d585450577fa0abc08715b2fd1ea50af99bceaea
cheise <cheise@localhost>
2012-01-04 01:26:41 -0600

    More subtle changes to file upload. Still not fully functioning

--- finalProject/inkle/databaseViews.py
+++ finalProject/inkle/databaseViews.py
@@ -37,7 +37,7 @@ def upload_image_view(request):
             destination.close()
         print "Copied image"
     
-    return 1
+    return HttpResponse()
 
 def edit_member_view(request):
     """Edits the logged in member's Member object."""
