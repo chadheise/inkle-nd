8d43cfb0314dec657083a8cb897d5e9b31ce6e1f
cheise <cheise@localhost>
2012-01-05 22:49:54 -0600

    Stopped trying to load inkle.forms in views.py and databaseViews.py

--- myproject/inkle/views.py
+++ myproject/inkle/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 from django.contrib import auth
 
 from myproject.inkle.models import *
-from myproject.inkle.forms import *
+#from myproject.inkle.forms import *
 from myproject.inkle.choices import *
 
 from django.db.models import Q
