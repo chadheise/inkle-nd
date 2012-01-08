76681553ddd1d64e1909e9111ed861383e6b5fbe
Jacob Wenger <wenger.jacob@gmail.com>
2012-01-08 15:18:09 -0500

    Cleaned up and commented models.py.

--- myproject/inkle/models.py
+++ myproject/inkle/models.py
@@ -2,8 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Location(models.Model):
+    """Location class definition."""
+    # General information
     name = models.CharField(max_length = 100)
-    image = models.CharField(max_length = 100, default = "default.jpg")
+    image = models.CharField(max_length = 100, default = "default.jpg") # TODO: change default to ""?
     category = models.CharField(max_length = 20, default = "Other")
     
     # Address
@@ -19,61 +21,71 @@ class Location(models.Model):
     def __unicode__(self):
         return "%s (%s, %s)" % (self.name, self.city, self.state)
 
+
 class Sphere(models.Model):
+    """Sphere class definition."""
     name = models.CharField(max_length = 50)
     
     def __unicode__(self):
         return "%s" % (self.name)
 
+
 class Circle(models.Model):
+    """Circle class definition."""
     name = models.CharField(max_length = 50)
     members = models.ManyToManyField("Member", symmetrical = False)
     
     def __unicode__(self):
         return "%s" % (self.name)
 
+
 class Follower(models.Model):
+    """Follower class definition."""
     follower = models.ForeignKey("Member")
     count = models.IntegerField()
     
     def __unicode__(self):
         return "%s (%d)" % (self.follower.username, self.count)
 
+
 class Inkling(models.Model):
+    """Inkling class definition."""
     location = models.ForeignKey(Location)
-    category = models.CharField(max_length = 1)
+    category = models.CharField(max_length = 20)
     date = models.CharField(max_length = 10)
     
     def __unicode__(self):
         return "%s (%s, %s)" % (self.location.name, self.date, self.category)
 
+
 class Member(User):
-    # User contains id, username, password, first_name, last_name, email, is_staff, is_active, is_superuser, last_login, and date_joined
-
+    """Member class definition. Inherits from built-in Django User class."""
+    circles = models.ManyToManyField(Circle, symmetrical = False)
     spheres = models.ManyToManyField(Sphere, symmetrical = False)
-    circles = models.ManyToManyField(Circle, symmetrical = False, related_name = "+")
-
-    pending = models.ManyToManyField("self", symmetrical = False, related_name = "++")
-    accepted = models.ManyToManyField("self", symmetrical = False, related_name = "+++")
-    requested = models.ManyToManyField("self", symmetrical = False, related_name = "++++")
-    followers = models.ManyToManyField(Follower, symmetrical = False)
-    following = models.ManyToManyField("self", symmetrical = False)
-    
     inklings = models.ManyToManyField(Inkling)
 
+    # Member lists
+    pending = models.ManyToManyField("self", symmetrical = False, related_name = "pending_members")
+    accepted = models.ManyToManyField("self", symmetrical = False, related_name = "accepted_members")
+    requested = models.ManyToManyField("self", symmetrical = False, related_name = "requested_members")
+    followers = models.ManyToManyField(Follower, symmetrical = False)
+    following = models.ManyToManyField("self", symmetrical = False, related_name = "following_members")
+    
+    # Profile information
     gender = models.CharField(max_length = 6)
-    birthday = models.CharField(max_length = 14)
+    birthday = models.CharField(max_length = 10)
     phone = models.CharField(max_length = 10, default = "")
-    image = models.CharField(max_length = 100, default = "default.jpg")
+    image = models.CharField(max_length = 100, default = "default.jpg") # TODO: change default to ""?
+    city = models.CharField(max_length = 50, default = "")
+    state = models.CharField(max_length = 2, default = "")
+    zip_code = models.CharField(max_length = 5, default = "")
     
     # Email verification
     verification_hash = models.CharField(max_length = 32)
     verified = models.BooleanField(default = False)
 
-    # Address
-    city = models.CharField(max_length = 50, default = "")
-    state = models.CharField(max_length = 2, default = "")
-    zip_code = models.CharField(max_length = 5, default = "")
-    
+    # Note: inherits from built-in Django User class which contains:
+    #       id, username, password, first_name, last_name, email, is_staff, is_active, is_superuser, last_login, and date_joined
+   
     def __unicode__(self):
-        return "%s" % (self.username)
+        return "%s (%s %s)" % (self.username, self.first_name, self.last_name)
