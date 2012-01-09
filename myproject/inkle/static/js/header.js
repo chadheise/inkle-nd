1ea7ff8805ca4302b361620904855e834c737de2
Jacob Wenger <wenger.jacob@gmail.com>
2012-01-09 13:15:04 -0500

    Added search button to search bar.

--- myproject/inkle/static/js/header.js
+++ myproject/inkle/static/js/header.js
@@ -1,29 +1,30 @@
 $(document).ready(function() {
     /* Initially, make the search input says "Search" and gray it out */
-    $("#searchInput").val("Search").addClass("emptySearchInput");
+    $("#headerSearchInput").val("Search").addClass("emptySearchInput");
     
     /* If the search input gains focus and it says "Search" grayed out, make the text black and empty it */
-    $("#searchInput").focus(function() {
+    $("#headerSearchInput").focus(function() {
         if ($(this).hasClass("emptySearchInput"))
         {
             $(this).val("").removeClass("emptySearchInput");
         }
     });
    
-    /* If the search input loses focus and is empty, gray it out and put "Search" in it  and fade out the search suggestions*/
-    $("#searchInput").blur(function() {
+    /* If the search input loses focus and is empty, gray it out, put "Search" in it, and fade out the search suggestions*/
+    $("#headerSearchInput").blur(function() {
         if ($(this).val() == "")
         {
             $(this).val("Search").addClass("emptySearchInput");
         }
         
-        $("#searchSuggestions").fadeOut("medium");
+        $("#headerSearchSuggestions").fadeOut("medium");
     });
 
-    $("#searchInput").keydown(function(e) {
+    /* If the "Enter" button is pressed and the search input is not empty, redirect to the search page */
+    $("#headerSearchInput").keydown(function(e) {
         if ((e.keyCode == 10) || (e.keyCode == 13))
         {
-            var query = $("#searchInput").val();
+            var query = $(this).val();
             query = query.replace(/^\s+|\s+$/g, "");
 
             if (query != "")
@@ -33,10 +34,22 @@ $(document).ready(function() {
         }
     });
     
-    $("#searchInput").keyup(function(e) {
+    /* If the search button is clicked and the search input is not empty, redirect to the search page */
+    $("#headerSearchButton").click(function() {
+        var query = $("#headerSearchInput").val();
+        query = query.replace(/^\s+|\s+$/g, "");
+
+        if ((query != "") && (!$("#headerSearchInput").hasClass("emptySearchInput")))
+        {
+            window.location.href = "/search/" + query;
+        }
+    });
+   
+    /* Update the search suggestions every time a key is pressed */
+    $("#headerSearchInput").keyup(function(e) {
         if ((e.keyCode != 10) && (e.keyCode != 13))
         {
-            var query = $("#searchInput").val();
+            var query = $(this).val();
 
             if (query != "")
             {
@@ -45,23 +58,24 @@ $(document).ready(function() {
                     url: "/suggestions/",
                     data: {"type" : "search", "query" : query},
                     success: function(html) {
-                        $("#searchSuggestions").html(html);
-                        $("#searchSuggestions").fadeIn("medium");
+                        $("#headerSearchSuggestions").html(html);
+                        $("#headerSearchSuggestions").fadeIn("medium");
                     },
                     error: function(a, b, error) {
                         ;
-                        //alert("header.js (1): " + error);
+                        alert("header.js (1): " + error);
                     }
                 });
             }
             else
             {
-                $("#searchSuggestions").fadeOut("medium");
+                $("#headerSearchSuggestions").fadeOut("medium");
             }
         }
     });
-    
-    $("#searchSuggestions .suggestion").live("click", function() {
+
+    /* Every time a search suggestion is clicked, redirect to the search page */
+    $("#headerSearchSuggestions .suggestion").live("click", function() {
         var query = $(this).children(".suggestion p").text();
         window.location.href = "/search/" + query;
     });
