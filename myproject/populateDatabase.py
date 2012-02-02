4da7b7895588509d6b66186f8ffdaf9e8dd4f5a8
jwenger <jwenger@localhost>
2012-02-02 16:31:07 -0500

    Added script to populate database.

---
+++ myproject/populateDatabase.py
@@ -0,0 +1,38 @@
+from inkle.models import *
+
+def load_dorms():
+    Location.objects.create(name = "Alumni Hall", category = "Dorm", street = "South Quad", city = "Notre Dame", state = "IN", zip_code = "46556", phone = "", website = "")
+    Location.objects.create(name = "Badin Hall", category = "Dorm", street = "South Quad", city = "Notre Dame", state = "IN", zip_code = "46556", phone = "", website = "")
+    Location.objects.create(name = "Breen Phillips Hall", category = "Dorm", street = "North Quad", city = "Notre Dame", state = "IN", zip_code = "46556", phone = "", website = "")
+    Location.objects.create(name = "Carroll Hall", category = "Dorm", street = "South Quad", city = "Notre Dame", state = "IN", zip_code = "46556", phone = "", website = "")
+    Location.objects.create(name = "Cavanaugh Hall", category = "Dorm", street = "North Quad", city = "Notre Dame", state = "IN", zip_code = "46556", phone = "", website = "")
+    Location.objects.create(name = "Dillon Hall", category = "Dorm", street = "South Quad", city = "Notre Dame", state = "IN", zip_code = "46556", phone = "", website = "")
+    Location.objects.create(name = "Duncan Hall", category = "Dorm", street = "West Quad", city = "Notre Dame", state = "IN", zip_code = "46556", phone = "", website = "")
+    Location.objects.create(name = "Farley Hall", category = "Dorm", street = "North Quad", city = "Notre Dame", state = "IN", zip_code = "46556", phone = "", website = "")
+    Location.objects.create(name = "Fisher Hall", category = "Dorm", street = "South Quad", city = "Notre Dame", state = "IN", zip_code = "46556", phone = "", website = "")
+    Location.objects.create(name = "Howard Hall", category = "Dorm", street = "South Quad", city = "Notre Dame", state = "IN", zip_code = "46556", phone = "", website = "")
+    Location.objects.create(name = "Keenan Hall", category = "Dorm", street = "North Quad", city = "Notre Dame", state = "IN", zip_code = "46556", phone = "", website = "")
+    Location.objects.create(name = "Keough Hall", category = "Dorm", street = "West Quad", city = "Notre Dame", state = "IN", zip_code = "46556", phone = "", website = "")
+    Location.objects.create(name = "Knott Hall", category = "Dorm", street = "Mod Quad", city = "Notre Dame", state = "IN", zip_code = "46556", phone = "", website = "")
+    Location.objects.create(name = "Lewis Hall", category = "Dorm", street = "North Quad", city = "Notre Dame", state = "IN", zip_code = "46556", phone = "", website = "")
+    Location.objects.create(name = "Lyons Hall", category = "Dorm", street = "South Quad", city = "Notre Dame", state = "IN", zip_code = "46556", phone = "", website = "")
+    Location.objects.create(name = "McGlinn Hall", category = "Dorm", street = "West Quad", city = "Notre Dame", state = "IN", zip_code = "46556", phone = "", website = "")
+    Location.objects.create(name = "Morrissey Hall", category = "Dorm", street = "South Quad", city = "Notre Dame", state = "IN", zip_code = "46556", phone = "", website = "")
+    Location.objects.create(name = "O'Neill Hall", category = "Dorm", street = "West Quad", city = "Notre Dame", state = "IN", zip_code = "46556", phone = "", website = "")
+    Location.objects.create(name = "Pangborn Hall", category = "Dorm", street = "South Quad", city = "Notre Dame", state = "IN", zip_code = "46556", phone = "", website = "")
+    Location.objects.create(name = "Pasquerilla East Hall", category = "Dorm", street = "Mod Quad", city = "Notre Dame", state = "IN", zip_code = "46556", phone = "", website = "")
+    Location.objects.create(name = "Pasquerilla West Hall", category = "Dorm", street = "Mod Quad", city = "Notre Dame", state = "IN", zip_code = "46556", phone = "", website = "")
+    Location.objects.create(name = "Ryan Hall", category = "Dorm", street = "West Quad", city = "Notre Dame", state = "IN", zip_code = "46556", phone = "", website = "")
+    Location.objects.create(name = "St. Edward's Hall", category = "Dorm", street = "South Quad", city = "Notre Dame", state = "IN", zip_code = "46556", phone = "", website = "")
+    Location.objects.create(name = "Siegfried Hall", category = "Dorm", street = "Mod Quad", city = "Notre Dame", state = "IN", zip_code = "46556", phone = "", website = "")
+    Location.objects.create(name = "Sorin Hall", category = "Dorm", street = "Main Quad", city = "Notre Dame", state = "IN", zip_code = "46556", phone = "", website = "")
+    Location.objects.create(name = "Stanford Hall", category = "Dorm", street = "North Quad", city = "Notre Dame", state = "IN", zip_code = "46556", phone = "", website = "")
+    Location.objects.create(name = "Walsh Hall", category = "Dorm", street = "Main Quad", city = "Notre Dame", state = "IN", zip_code = "46556", phone = "", website = "")
+    Location.objects.create(name = "Welsh Family Hall", category = "Dorm", street = "West Quad", city = "Notre Dame", state = "IN", zip_code = "46556", phone = "", website = "")
+    Location.objects.create(name = "Zahm Hall", category = "Dorm", street = "North Quad", city = "Notre Dame", state = "IN", zip_code = "46556", phone = "", website = "")
+
+
+def load_campus_locations():
+    Location.objects.create(name = "LaFortune Student Center", category = "Other", street = "Main Quad", city = "Notre Dame", state = "IN", zip_code = "46556", phone = "", website = "")
+    Location.objects.create(name = "Joyce Athletic and Convocation Center (JACC)", category = "Arena", street = "Main Quad", city = "Notre Dame", state = "IN", zip_code = "46556", phone = "", website = "")
+    Location.objects.create(name = "Notre Dame Stadium", category = "Arena", street = "Main Quad", city = "Notre Dame", state = "IN", zip_code = "46556", phone = "", website = "")
